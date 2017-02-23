828b1048afe29a59471ad1ca2dc7c6724a3a8115
FSEVERI\grecu3237 <FSEVERI\grecu3237@linf1-23.fseveri.it>
2017-02-23 12:05:19 +0100

    aggiunto posizione

--- src/deserto/pos/posizione.java
+++ src/deserto/pos/posizione.java
@@ -5,10 +5,59 @@
  */
 package deserto.pos;
 
+import static java.lang.Math.random;
+
 /**
  *
  * @author FSEVERI\bollettin3412
  */
 public class posizione {
+    private int x;
+    private int y;
+    
+    /**
+     * Costruttore
+     */
+    public posizione (){
+        x=(int) Math.random()*10;
+        y=(int) Math.random()*10;
+    }
+    
+    /**
+     * Metofo getter di x
+     * @return Il valore di x
+     */
+    public int getX() {
+        return x;
+    }
+    /**
+     * Getter di y
+     * @return Il valore di y
+     */
+    public int getY() {
+        return y;
+    }
+    /**
+     * Metodo equals della classe
+     * @param obj Oggetto da confrontare
+     * @return True se sono uguali, false altrimenti
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final posizione other = (posizione) obj;
+        if (this.x != other.x) {
+            return false;
+        }
+        if (this.y != other.y) {
+            return false;
+        }
+        return true;
+    }
     
 }

53e716317b412bfb81a3ee97101395783b729943
FSEVERI\grecu3237 <FSEVERI\grecu3237@linf1-23.fseveri.it>
2017-02-23 12:27:08 +0100

    Aggiuto Carro Cantiere

--- src/deserto/carri/CarroCantiere.java
+++ src/deserto/carri/CarroCantiere.java
@@ -5,10 +5,24 @@
  */
 package deserto.carri;
 
+import deserto.pos.Pezzo;
+import deserto.pos.Posizione;
+import java.util.ArrayList;
 /**
  *
  * @author FSEVERI\bollettin3412
  */
-public class CarroCantiere {
+public abstract class CarroCantiere {
+    private int npezzi;
+    private Posizione pos;
+    private ArrayList<Pezzo> carro;
     
+    public CarroCantiere (Posizione pos, int npezzi){
+        this.pos=pos;
+        this.npezzi=npezzi;
+        carro = new ArrayList<Pezzo>(npezzi);
+    }
+    public abstract int stato();
+    public abstract boolean distrutto();
+    public abstract void fuoco(Posizione pos);
 }

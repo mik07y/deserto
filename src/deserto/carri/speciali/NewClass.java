1282421b59e314fd142e0756b3a897d5c79fdcfc
FSEVERI\bollettin3412 <FSEVERI\bollettin3412@linf1-24.fseveri.it>
2017-02-23 11:48:20 +0100

    gtgtt

--- src/deserto/carri/speciali/NewClass.java
+++ src/deserto/carri/speciali/NewClass.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package deserto.carri;
+package deserto.carri.speciali;
 
 /**
  *
  * @author FSEVERI\bollettin3412
  */
-public class CarroCantiere {
+public class NewClass {
     
 }
